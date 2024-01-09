8dd43163bd2d195e4181845ea0bcc0c6678880e6
imerla1 <giorgi1048@yahoo.com>
2024-01-09 18:00:54 +0400

    add root directory global variable.

--- gui/globals.py
+++ gui/globals.py
@@ -1,7 +1,8 @@
 from logger_config import configure_logger
 from security.secure_encryptor import SecureCrypto
+import os
 
-
+root_directory = os.getcwd()
 main_logger = configure_logger()
 crypto_secret_key = b'O6vJtGEZmrq5cOhcfftPfwOCcAvfcG5hnIdgHO_NvUk='
 secure_crypto = SecureCrypto(crypto_secret_key)
